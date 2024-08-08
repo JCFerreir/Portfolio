c708c9e8602a6ee9bc49f3d10c3808f62fd1920f
Julio Cesar dos Santos Ferreira <juliocesar.ferreira359@outlook.com>
2024-08-08 19:00:37 -0300

    Normalize line endings

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header-portfolio';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importa o JavaScript do Bootstrap
+
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+}
+
+export default App;
