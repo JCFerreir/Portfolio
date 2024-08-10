549a21f20be08c454365d41242af834a77af66a7
Julio Cesar dos Santos Ferreira <juliocesar.ferreira359@outlook.com>
2024-08-10 17:01:50 -0300

    o header está pronto, faltando apenas fazer o modal de contatos sincronizar com o botão

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header-portfolio';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importa o JavaScript do Bootstrap
 
--- src/components/Header-portfolio.js
+++ src/components/Header-portfolio.js
@@ -2,38 +2,53 @@ import React from "react";
 import './Header-portfolio.css'
 
 const Header = () => {
-    return (
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Expand at lg</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+  return (
+    <>
+      <header>
+        <div className="interface">
+            <div className="logo">
+              <a href=""> <img src="/logo-portfolio.png" /></a>
+            </div>
 
-      <div class="navbar-collapse collapse" id="navbarsExample05">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <nav className="menu-desktop">
+            <ul>
+              <li><a href="teste">Teste</a></li>
+              <li><a href="teste">Teste</a></li>
+              <li><a href="teste">Teste</a></li>
             </ul>
-          </li>
-        </ul>
+        </nav>
+
+        <div className="btn-contato"> 
+          <a href="">
+            <button  data-bs-toggle="modal" data-bs-target="#exampleModal">Contato</button>
+          </a>
+        </div>
+      </div>
+      </header>
+
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
-  </nav>
-    );    
+  </div>
+</div>
+    </>
+  );
 };
 
 export default Header;
