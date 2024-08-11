d66526d78a2b4b34fc16a89466897c2c76dc8ada
Julio Cesar dos Santos Ferreira <juliocesar.ferreira359@outlook.com>
2024-08-10 23:05:36 -0300

    terminei o footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Header from './components/Header-portfolio';
+import Footer from './components/Footer-portfolio'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importa o JavaScript do Bootstrap
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Footer />
         </div>
     );
 }
