84cd4704be921edca9df393b62cf6e04d1b426a6
Julio Cesar dos Santos Ferreira <juliocesar.ferreira359@outlook.com>
2024-09-04 19:25:49 -0300

    Feito 90% do 'sobre mim'

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -23,6 +23,24 @@ const Welcome = () => {
             delay: 500,
             origin: 'right'
         });
+        revelar.reveal('.titulo-sobre-mim', {
+            duration: 1000,
+            distance: '90px',
+            delay: 500,
+            origin: 'left'
+        });
+        revelar.reveal('.texto-sobre-mim', {
+            duration: 1000,
+            distance: '90px',
+            delay: 800,
+            origin: 'left'
+        });
+        revelar.reveal('.imagem-sobre-mim', {
+            duration: 1000,
+            distance: '90px',
+            delay: 1000,
+            origin: 'right'
+        });
     }, []); // O array vazio garante que o efeito só será executado após a montagem do componente
 
     return (
@@ -61,12 +79,26 @@ const Welcome = () => {
                 </div>
             </section>
 
-            <section className="about-me" id="sobre-mim">
+            <section className="sobre-mim" id="sobre-mim">
             <svg className="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1f1f1f" fill-opacity="1" d="M0,64L60,96C120,128,240,192,360,218.7C480,245,600,235,720,192C840,149,960,75,1080,48C1200,21,1320,43,1380,53.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-                <div className="conteudo-sobre-mim">
-                    <h1 className="titulo-sobre-mim">Sobre Mim!</h1>
+                
+                <h1 className="titulo-sobre-mim">Sobre Mim!</h1>
 
-                    
+                <div className="conteudo-sobre-mim">
+
+                    <div className="texto-sobre-mim">
+                        Sou um programador front-end especializado <br />
+                            em desenvolvimento de interfaces intuitivas e <br />
+                            responsivas. Busco constantemente aprimorar <br />
+                            minhas habilidades para criar soluções web de <br />
+                            alta qualidade, com foco em desempenho e <br />
+                            usabilidade.
+                    </div>
+
+                    <div className="imagem-sobre-mim">
+                        <img src="/foto-para-portfolio-03_09.jpg"></img>
+                    </div>
+
                 </div>
             <svg className="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1f1f1f" fill-opacity="1" d="M0,64L60,96C120,128,240,192,360,218.7C480,245,600,235,720,192C840,149,960,75,1080,48C1200,21,1320,43,1380,53.3L1440,64L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
             </section>
